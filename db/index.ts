677aeeada68656d451e5e2c39096536454a5cbec
hejmarcin29 <hejmarcin29@gmail.com>
2025-11-09 23:09:21 +0100

    chore: remove repository contents

--- db/index.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
-
-const databaseUrl = process.env.DATABASE_URL ?? "file:./data/panel.db";
-
-const client = createClient({ url: databaseUrl });
-
-export const db = drizzle(client);
--- src/app/(app)/montaze/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function MontazePage() {
-  redirect("/dropshipping");
-}
