8e1abfbc8f2446dbc54e0adf3c1f7beae5c420a5
hejmarcin29 <hejmarcin29@gmail.com>
2025-11-10 14:47:29 +0100

    updat15

--- src/app/dashboard/orders/manual-order-form-client.tsx
+++ src/app/dashboard/orders/manual-order-form-client.tsx
@@ -21,7 +21,6 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from '@/components/ui/select';
-import { Separator } from '@/components/ui/separator';
 import { Switch } from '@/components/ui/switch';
 import { Textarea } from '@/components/ui/textarea';
 
