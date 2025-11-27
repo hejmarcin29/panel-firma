6af08210af444bf1657a612db5856fd110ae2ea2
hejmarcin29 <hejmarcin29@gmail.com>
2025-11-27 10:06:31 +0100

    update92

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    // Opcjonalne: Zmniejsza zużycie pamięci kosztem czasu budowania
+    workerThreads: false,
+    cpus: 1,
+  },
 };
 
 export default nextConfig;
