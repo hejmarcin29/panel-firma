794c581c5823d423b2975d8a4a83646d81a399b2
hejmarcin29 <hejmarcin29@gmail.com>
2025-11-27 23:25:00 +0100

    Add generated migrations

---
+++ drizzle/0022_tearful_squadron_supreme.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `montages` ADD `measurement_details` text;--> statement-breakpoint
+ALTER TABLE `montages` ADD `panel_type` text;--> statement-breakpoint
+ALTER TABLE `montages` ADD `additional_info` text;--> statement-breakpoint
+ALTER TABLE `montages` ADD `forecasted_installation_date` integer;
