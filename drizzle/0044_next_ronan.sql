4762e5fac43d8040dd54770ac9d1674ee51ffe51
hejmarcin29 <hejmarcin29@gmail.com>
2025-12-27 15:50:22 +0100

    Add generated migrations

---
+++ drizzle/0044_next_ronan.sql
@@ -0,0 +1,13 @@
+ALTER TABLE "montages" DROP COLUMN "scheduled_skirting_installation_at";--> statement-breakpoint
+ALTER TABLE "montages" DROP COLUMN "scheduled_skirting_installation_end_at";--> statement-breakpoint
+ALTER TABLE "montages" DROP COLUMN "measurement_separate_skirting";--> statement-breakpoint
+ALTER TABLE "montages" DROP COLUMN "skirting_length";--> statement-breakpoint
+ALTER TABLE "montages" DROP COLUMN "skirting_details";--> statement-breakpoint
+ALTER TABLE "montages" DROP COLUMN "skirting_model";--> statement-breakpoint
+ALTER TABLE "montages" DROP COLUMN "skirting_product_id";--> statement-breakpoint
+ALTER TABLE "montages" DROP COLUMN "skirting_waste";--> statement-breakpoint
+ALTER TABLE "montages" DROP COLUMN "final_skirting_length";--> statement-breakpoint
+ALTER TABLE "montages" DROP COLUMN "skirting_material_status";--> statement-breakpoint
+ALTER TABLE "montages" DROP COLUMN "skirting_material_claim_type";--> statement-breakpoint
+ALTER TABLE "montages" DROP COLUMN "skirting_protocol_data";--> statement-breakpoint
+ALTER TABLE "montages" DROP COLUMN "skirting_client_signature_url";
